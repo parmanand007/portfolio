59914905e3398f4f4af00ccd11b840f6d005dbca
parmanand007 <parmanand.p@brimo.in>
2023-10-04 00:30:29 +0530

    created projection section

--- components/about.tsx
+++ components/about.tsx
@@ -1,18 +1,23 @@
-"use client"
-import React from 'react'
-import SectionHeading from './section-heading'
-import {motion} from "framer-motion"
+"use client";
 
-const About = () => {
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+export default function About() {
+ const {ref,inView} =useInView();
+ 
   return (
-    <motion.section 
-    initial={{opacity:0,y:100}}
-    animate={{opacity:1,y:0}}
-    transition={{duration:0.5}}
-
-    className='mb-2 8 max-w-[45rem] text-center leading-8 sm:mb-40'>
-        <SectionHeading>About Me</SectionHeading>
-        <p className='mb-3'>
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }} 
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading> 
+      <p className="mb-3">
         After graduating with a degree in{" "}
         <span className="font-medium">Accounting</span>, I decided to pursue my
         passion for programming. I enrolled in a coding bootcamp and learned{" "}
@@ -28,17 +33,16 @@ const About = () => {
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
-        </p>
-        <p>
+      </p>
+
+      <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
         video games, watching movies, and playing with my dog. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
         <span className="font-medium">history and philosophy</span>. I'm also
         learning how to play the guitar.
-        </p>
-        </motion.section>
-  )
+      </p>
+    </motion.section>
+  );
 }
-
-export default About
--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,12 @@
 "use client"
 import React from 'react'
-import {motion} from "framer-motion";
+import {motion} from "framer-motion"; 
 import { links } from '@/lib/data';
 import Link from 'next/link';
+import clsx from "clsx"
+import {  useActiveSectionContext } from '@/context/active-section-context';
 const Header = () => {
+ const {activeSection,setActiveSection}=useActiveSectionContext()
   return (
     <header className='z-[999] relative'>
     <motion.div className="fixed top-0 left-1/2  h-[4.5rem] w-full rounded-none border
@@ -22,15 +25,28 @@ const Header = () => {
            {
            links.map(link=>(
             <motion.li 
-            className='h-3/4 flex items-center justify-center'
+            className='h-3/4 flex items-center justify-center relative'
             key={link.hash}
             initial={{y:-100,opacity:0}}
             animate={{y:0,opacity:1}}
             >
-                <Link className='flex w-full items-center justify-center 
-                px-3 py-3 hover:text-gray-950 transition
-                ' href={link.hash} >
+                <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition ',{"text-gray-950":activeSection ===link.name})} href={link.hash}
+                onClick={()=>setActiveSection(link.name)}
+                >
                  {link.name}
+                 {
+                  link.name===activeSection && (
+<motion.span 
+layoutId='activeSection'
+transition={{
+  type:"spring",
+  stiffness:380,
+  damping:30
+}}
+className='bg-gray-100 rounded-full absolute inset-0 -z-10'></motion.span>
+                  )
+                 }
+                 
                 </Link>
             </motion.li>
            ))
