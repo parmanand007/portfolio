b5a89828587454a5070e51da5a74ea28f3e127d9
parmanand007 <parmanand.p@brimo.in>
2023-10-04 00:57:44 +0530

    set active link of nav bar1

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -7,15 +7,18 @@ type ActiveSectionContextProviderProps ={
 }
 type ActiveSectionContextType={
     activeSection:SectionName,
-    setActiveSection:React.Dispatch<React.SetStateAction<SectionName>>
+    setActiveSection:React.Dispatch<React.SetStateAction<SectionName>>,
+    timeOfLastClick:number,
+    setTimeOfLastClick:React.Dispatch<React.SetStateAction<number>> 
 }
  
 export const ActiveSectionContext = createContext<ActiveSectionContextType | null>(null)
 
 const ActiveSectionContextProvider = ({children}:ActiveSectionContextProviderProps) => {
     const [activeSection,setActiveSection]= useState<SectionName >('Home');
+    const [timeOfLastClick,setTimeOfLastClick] =useState(0)
     return (
-        <ActiveSectionContext.Provider value={{activeSection,setActiveSection}}>
+        <ActiveSectionContext.Provider value={{activeSection,setActiveSection,timeOfLastClick,setTimeOfLastClick}}>
         {children}
         </ActiveSectionContext.Provider>
         
