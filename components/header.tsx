8aff7565ae8135620183c3cf26b5aa222d82e900
parmanand007 <parmanand.p@brimo.in>
2023-10-08 11:06:21 +0530

    dark mode functionality is added

--- components/header.tsx
+++ components/header.tsx
@@ -11,8 +11,8 @@ const Header = () => {
     <header className='z-[999] relative'>
     <motion.div className="fixed top-0 left-1/2  h-[4.5rem] w-full rounded-none border
      border-white border-opacity-40 bg-white bg-opacity-80 shadow-black/[0.03]
-     backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
-    "
+     backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opactity-75
+    " 
     initial={{y:-100,x:"-50%",opacity:0}}
     animate={{y:0,x:"-50%",opacity:1}}
     ></motion.div>
@@ -25,12 +25,12 @@ const Header = () => {
            {
            links.map(link=>(
             <motion.li 
-            className='h-3/4 flex items-center justify-center relative'
+            className='h-3/4 flex items-center justify-center relative '
             key={link.hash}
             initial={{y:-100,opacity:0}}
             animate={{y:0,opacity:1}}
             >
-                <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition ',{"text-gray-950":activeSection ===link.name})} href={link.hash}
+                <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition  dark:hover:text-gray-300',{"text-gray-950 dark:text-gray-200":activeSection ===link.name})} href={link.hash}
                 onClick={()=>{
                   setActiveSection(link.name);setTimeOfLastClick(Date.now())
                 }
@@ -46,7 +46,7 @@ transition={{
   stiffness:380,
   damping:30
 }}
-className='bg-gray-100 rounded-full absolute inset-0 -z-10'></motion.span>
+className='bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800'></motion.span>
                   )
                  }
                  
