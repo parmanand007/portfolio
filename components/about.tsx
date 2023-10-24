e41bbda5413aac087d2579c77177afc8aa133772
parmanand007 <parmanand.p@brimo.in>
2023-10-24 15:39:07 +0530

    add details and picture of me

--- components/about.tsx
+++ components/about.tsx
@@ -18,18 +18,18 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading> 
       <p className="mb-3">
-        After graduating with a degree in{" "}
-        <span className="font-medium">Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        After post-graduating with a degree in{" "}
+        <span className="font-medium">Master of Computer Applications</span>, I decided to pursue my
+        passion for web development and mobile app development. I created multiple projects and learned{" "}
+        <span className="font-medium"> web development</span>.{" "}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          Django, Python, GraphQL, Postgresql
         </span>
-        . I am also familiar with TypeScript and Prisma. I am always looking to
+        . I am also familiar with tech like Flutter, React, Next.js, JavaScript, and MongoDB,Appwrite and Prisma and used tools like Docker, Jenkins, Graylog & Redash. I am always looking to
         learn new technologies. I am currently looking for a{" "}
         <span className="font-medium">full-time position</span> as a software
         developer.
@@ -37,11 +37,10 @@ export default function About() {
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        video games, watching movies & animes. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning about{" "}
-        <span className="font-medium">history and philosophy</span>. I'm also
-        learning how to play the guitar.
+        <span className="font-medium">Dev Ops role and their tools</span>. I always  work on small projects of different tech in free time.
       </p>
     </motion.section>
   );
